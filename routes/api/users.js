14796b8d3f36f88dc2e499994712ed552c44bd77
thisisharrison <harrison.lau@outlook.com>
2021-01-22 19:44:35 +0800

    Json web token added

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 const User = require('../../models/User');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
@@ -26,11 +28,49 @@ router.post("/register", (req, res) => {
                         if (err) throw err;
                         newUser.password = hash;
                         newUser.save()
-                            .then(user => res.json(user))
+                            .then(user => {
+                                // Add token
+                                const payload = { id: user.id, handle: user.handle };
+                                jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+                                    res.json({
+                                        success: true,
+                                        token: 'Bearer ' + token
+                                    });
+                                });
+                            })
                             .catch(err => console.log(err));
                     })
                 });
             }
+        });
+});
+
+// Controller for login
+router.post("/login", (req, res) => {
+    const { email, password } = req.body;
+    User.findOne({ email: email})
+        .then(user => {
+            if (!user) {
+                return res.status(404).json({ email: "User does not exist" });
+            } 
+            // Check password
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        // Add webtoken
+                        const payload = { id: user.id, handle: user.handle };
+                        // Add webtoken so user stays signed in across multiple requests to backend
+                        jwt.sign(
+                            payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+                                res.json({
+                                    success: true,
+                                    token: 'Bearer ' + token
+                                });
+                            });
+                    } else {
+                        return res.status(400).json({ password: "Incorrect password" });
+                    }
+                })
         });
 });
 
